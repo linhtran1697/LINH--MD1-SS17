5efd5d2c0e9cc1fe0f646e9ea3298cdece25f513
linhtran1697 <linhtran.1697.lt@gmail.com>
2026-06-08 20:13:44 +0900

    Add files via upload

---
+++ SS17 -EX5/SS17-EX5.js
@@ -0,0 +1,13 @@
+document.getElementById("login").onclick = function(){
+let username = document.getElementById("user").value;
+let password = document.getElementById("pass").value;
+if(username == "huanrose@gmail.com" && password == "123456"){
+alert("Đăng nhập thành công");
+}else{
+alert("Đăng nhập thất bại");
+}
+}
+
+
+   
+           
